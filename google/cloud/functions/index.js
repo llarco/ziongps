288f9b285bd6a597f71b181a22603298235712ea
Luis Larco <luislarco@luiss-air.lan>
2019-03-05 22:20:59 -0800

    Support Data object

--- google/cloud/functions/index.js
+++ google/cloud/functions/index.js
@@ -1,12 +1,4 @@
-/**
- * Google Cloud Function used to read a Cloud Pub/Sub message and store it into
- * a ziongps datastore entity.
- *
- * @author Luis Larco <luis@luislarco.com>
- */
- 
 const ds = require('@google-cloud/datastore')();
-var key = ds.key('ziongps');
 
 /**
  * Triggered from a message on a Cloud Pub/Sub topic.
@@ -17,14 +9,37 @@ var key = ds.key('ziongps');
 exports.subscribe = function subscribe(event, callback) {
   // The Cloud Pub/Sub Message object.
   const pubsubMessage = event.data;
-  
+
   if (!pubsubMessage.data) {
     callback();
     return;
   }
-  
-  var entry = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString())
-  
+
+  var decodedMessage = Buffer.from(pubsubMessage.data, 'base64').toString();
+  if (decodedMessage == 'test-event') {
+    callback();
+    return;
+  }
+
+  var entry;
+  try {
+    entry = JSON.parse(decodedMessage);
+  } catch (e) {
+    console.log('Unable to parse JSON');
+    console.log('data = ' + pubsubMessage.data);
+    console.log('decodedMessage = ' + decodedMessage);
+    throw e;
+  }
+
+  var date;
+  try {
+    date = new Date(pubsubMessage.attributes.published_at);
+  } catch (e) {
+    console.log('Unable to build Date');
+    console.log('published_at = ' + pubsubMessage.attributes.published_at);
+   	throw e;
+  }
+
   var obj = {
     gc_pub_sub_id: pubsubMessage.id,
     device_id: pubsubMessage.attributes.device_id,
@@ -32,19 +47,22 @@ exports.subscribe = function subscribe(event, callback) {
     lat: entry.lat,
     lon: entry.lon,
     age: entry.age,
-    published_at: pubsubMessage.attributes.published_at
-  }
-  
+    published_at: date
+  };
+
+  var key = ds.key(['ziongps', obj.published_at]);
+
   ds.save({
     key: key,
     data: obj
   }, function(err) {
     if(err) {
       console.log('There was an error storing the event: ' + obj, err);
+    } else {
+      console.log('zion stored: ' + JSON.stringify(obj));
     }
-    console.log('zion stored: ' + obj)
   });
-  
+
   // Don't forget to call the callback.
   callback();
 };
